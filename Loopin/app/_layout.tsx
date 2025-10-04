f5edad363a23b3bd8263bda2d25309ffb4862ff1
ebrahim <ebuebrahim786@gmail.com>
2025-10-04 10:30:50 +0800

    Testing Local Biometrics authentication

--- Loopin/app/_layout.tsx
+++ Loopin/app/_layout.tsx
@@ -6,8 +6,10 @@ import {
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import * as LocalAuthentication from "expo-local-authentication";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useEffect } from "react";
 
 export const unstable_settings = {
   anchor: "/",
@@ -16,6 +18,17 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    const func = async () => {
+      const hasHardware = await LocalAuthentication.hasHardwareAsync();
+      const supportedTypes =
+        await LocalAuthentication.supportedAuthenticationTypesAsync();
+      const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+      const result = await LocalAuthentication.authenticateAsync()
+      console.log("AUTHEN:", hasHardware, supportedTypes, isEnrolled, result);
+    };
+    func();
+  });
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
