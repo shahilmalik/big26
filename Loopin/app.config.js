9e42773ee5b9de110b9f56fd227fab14570a23e1
ebrahim <ebuebrahim786@gmail.com>
2025-10-05 23:10:38 +0800

    frontend fix

--- Loopin/app.config.js
+++ Loopin/app.config.js
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
-
+// import "dotenv/config";
 // Load the .env.expo file explicitly
 dotenv.config({ path: ".env.expo" });
 
 export default {
   expo: {
-    name: "Proximum",           
-    slug: "proximum",         
-    version: "1.0.0",              
-    sdkVersion: "54.0.0",          
+    name: "Proximum",
+    slug: "proximum",
+    version: "1.0.0",
+    sdkVersion: "54.0.0",
     extra: {
       API_URL: process.env.API_URL,
     },
--- Loopin/app/login.tsx
+++ Loopin/app/login.tsx
@@ -30,10 +30,12 @@ function Login() {
   const [screen, setScreen] = useState<"home" | "signin" | "signup">("home");
   return (
     <SafeAreaView
+      edges={["top"]}
       style={{
         display: "flex",
         justifyContent: "flex-end",
-        backgroundColor: colors.background,
+        backgroundColor: debug ? "green" : "#22333b",
+        // backgroundColor: colors.background,
         height: "100%",
       }}
     >
@@ -43,39 +45,99 @@ function Login() {
           style={{
             backgroundColor: debug ? "red" : "",
             gap: "20",
-            padding: 10,
+            padding: 0,
             justifyContent: "space-between",
             flex: 1,
           }}
         >
           <View
             style={{
-              backgroundColor: debug ? "yellow" : "",
-              flex: 1,
+              flex: 3,
               justifyContent: "center",
               alignItems: "center",
+              backgroundColor: debug ? "red" : "",
             }}
           >
-            <Text style={{ color: colors.text, fontSize: 24 }}>
-              Welcom To Our Application
-            </Text>
+            <ThemedText
+              style={{
+                fontSize: 30,
+                fontWeight: "bold",
+                padding: 5,
+                textAlign: "center",
+              }}
+            >
+              Change Starts Here,
+            </ThemedText>
+            <ThemedText
+              style={{
+                fontSize: 30,
+                fontWeight: "bold",
+                padding: 5,
+                textAlign: "center",
+              }}
+            >
+              Spreads Everywhere
+            </ThemedText>
           </View>
-          <View style={{ gap: 10, backgroundColor: debug ? "pink" : "" }}>
-            <CustomButton
-              title="SIGN IN"
-              bgColor="black"
-              color="white"
-              onPress={() => setScreen("signin")}
-            />
-            <CustomButton
-              title="CREATE ACCOUNT"
-              color="black"
-              onPress={() => setScreen("signup")}
-            />
-            <CustomButton
-              title={`DEV: ${themeName}`}
-              onPress={() => toggleTheme()}
-            />
+          <View
+            style={{
+              flex: 2,
+              borderTopLeftRadius: 40,
+              borderTopRightRadius: 40,
+              borderColor: "yellow",
+              // borderWidth: 2,
+              borderBottomWidth: 0,
+              backgroundColor: debug ? "blue" : "#eae0d6",
+              padding: 20,
+              paddingBottom: 30,
+            }}
+          >
+            <View
+              style={{
+                // backgroundColor: debug ? "yellow" : "",
+                flex: 2,
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 20,
+              }}
+            >
+              <Text
+                style={{ color: "black", fontWeight: "bold", fontSize: 24 }}
+              >
+                Proximum
+              </Text>
+              <Text style={{ textAlign: "center" }}>
+                See what’s happening in your city, state, or country. Chat with
+                nearby users, join local groups, and share your
+                thoughts—anonymously or openly. With location-based chats and
+                updates, Proximum keeps you connected to the world around you.
+              </Text>
+            </View>
+            <View
+              style={{
+                gap: 10,
+                backgroundColor: debug ? "pink" : "",
+                display: "flex",
+                flexDirection: "row",
+                width: "50%",
+              }}
+            >
+              <CustomButton
+                title="SIGN IN"
+                bgColor="black"
+                color="white"
+                onPress={() => setScreen("signin")}
+              />
+              <CustomButton
+                title="CREATE ACCOUNT"
+                color="black"
+                onPress={() => setScreen("signup")}
+              />
+              {/* <CustomButton
+                title={`DEV: ${themeName}`}
+                onPress={() => toggleTheme()}
+              /> */}
+            </View>
           </View>
         </View>
       )}
